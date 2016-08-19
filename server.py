4bfc6f586d5c5ed44b6a19426db47c2daeea4124
Rachel Traina-Grandon <racheltrainagrandon@gmail.com>
2016-08-19 02:49:36 +0000

    Added new models to server file.

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ from jinja2 import StrictUndefined
 from flask import Flask, render_template, request, flash, redirect, session
 from flask_debugtoolbar import DebugToolbarExtension
 
-from model import connect_to_db, db, Encounter, Location, Gym, User
+from model import connect_to_db, db, Encounter, Location, User, Gym, PokeType, PokeBase, TypeBase
 
 
 app = Flask(__name__)
